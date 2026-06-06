6bb0883a1c9a3154c37e08e862b0c55a0cac0316
eggsonafryingpan <eggsonafryingpan@gmail.com>
2026-06-06 19:29:51 -0400

    vvcxv

--- IHave27Pickles/src/app/api/wiki/route.js
+++ IHave27Pickles/src/app/api/wiki/route.js
@@ -10,28 +10,30 @@ export async function GET(req) {
     if (now - lastCall < 100) return;
     lastCall = now;
 
-    let cache;
-    try {
-        let file = fs.readFileSync(process.cwd() + "/cache.json", "utf-8");
-        cache = file ? JSON.parse(file) : {};
-    } catch {
-        cache = {};
-    }
+    // let cache;
+    // try {
+    //     let file = fs.readFileSync(process.cwd() + "/cache.json", "utf-8");
+    //     cache = file ? JSON.parse(file) : {};
+    // } catch {
+    //     cache = {};
+    // }
 
-    if (cache[url]) {
-        return Response.json(cache[url]);
-    }
+    // if (cache[url]) {
+    //     return Response.json(cache[url]);
+    // }
 
 
-    const response = await axios.get(url, {
-        headers: {
-            "User-Agent":
-                "WikiScrapingTest/1.0 (educational use)",
-        },
-    });
-    cache[url] = response.data;
+    const response = await axios.get(url,
+        {
+            headers: {
+                "User-Agent":
+                    "WikiScrapingTest/1.0 (educational use)",
+            },
+        }
+    );
+    // cache[url] = response.data;
 
-    // await new Promise(r => setTimeout(r, 1000)); PLEASE DO IT SLOW
-    fs.writeFileSync(process.cwd() + "/cache.json", JSON.stringify(cache, null, 2));
+    // // await new Promise(r => setTimeout(r, 1000)); PLEASE DO IT SLOW
+    // fs.writeFileSync(process.cwd() + "/cache.json", JSON.stringify(cache, null, 2));
     return Response.json(response.data);
 }
